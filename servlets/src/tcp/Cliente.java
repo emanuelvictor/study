a1d664e9553019358f5469eb86eac0d3a4784745
Emanuel Victor <emanuelvictor@msn.com>
2019-04-01 19:36:20 -0300

    IMplementando aula inicial de java

---
+++ servlets/src/tcp/Cliente.java
@@ -0,0 +1,43 @@
+package tcp;
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Cliente {
+
+    private static Socket conexao;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+
+        try {
+            // Solicitar conexão
+            conexao = new Socket("127.0.0.1", 4000);
+
+
+            // Enviar dados
+            saida = new DataOutputStream(conexao.getOutputStream());
+
+            int dados = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor", entrada));
+            saida.writeInt(dados);
+
+            // Receber resposta
+            entrada = new DataInputStream(conexao.getInputStream());
+
+            final String resultado =  entrada.readUTF();
+            System.out.println("Servidor enviou: " + resultado);
+
+            // Fechar conexão
+            conexao.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ servlets/src/tcp/Servidor.java
@@ -0,0 +1,43 @@
+package tcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+    private static Socket conexao;
+    private static ServerSocket servidor;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+
+        try {
+
+            // Reservar uma porta
+            servidor = new ServerSocket(4000);
+
+            // Aguardar conexões
+            System.out.println("Aguardando conexão");
+            conexao = servidor.accept();
+
+            // Receber dados
+            entrada = new DataInputStream(conexao.getInputStream());
+            final int valor = entrada.readInt();
+
+            // Responder cliente
+            final String retorno = valor > 0 ? "O valor é maior que zero" : "O valor é menor que zero";
+            saida = new DataOutputStream(conexao.getOutputStream());
+            saida.writeUTF("retorno");
+
+            // Fecha conexão
+            conexao.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
