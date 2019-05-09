677cf234163ee3ec559839b4f7bb5fdf945927df
emanuelvictor <emanuelvictor@msn.com>
2019-05-08 22:54:28 -0300

    Alternado pacotes

---
+++ frameworks/books/src/main/ts/src/sistema/domain/presentation/cadastros/editora/editora-view.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'editora-view',
+  templateUrl: './editora-view.component.html',
+  styleUrls: ['./editora-view.component.scss']
+})
+export class EditoraViewComponent {
+
+  constructor() {
+  }
+
+}
