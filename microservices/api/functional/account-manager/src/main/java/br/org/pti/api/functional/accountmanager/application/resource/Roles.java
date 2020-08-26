57cd894a6b5ae5530f800b84aa6d370ec9520155
emanuel.fonseca <emanuel.fonseca@pti.org.br>
2020-08-26 11:16:27 -0300

    Fix Roles

--- microservices/api/functional/account-manager/src/main/java/br/org/pti/api/functional/accountmanager/application/resource/Roles.java
+++ microservices/api/functional/account-manager/src/main/java/br/org/pti/api/functional/accountmanager/application/resource/Roles.java
@@ -9,38 +9,44 @@ package br.org.pti.api.functional.accountmanager.application.resource;
  */
 public class Roles {
 
-    private static final String API = "/";
+    private static final String SEPARATOR = "/";
+
+    private static final String ROOT = "root";
+
+    private static final String APP_IDENTIFIER = "account-manager";
 
     /**
      * Grupos de acesso
      */
     private static final String ACCESS_GROUP_MAPPING = "access-groups";
-    static final String ACCESS_GROUP_MAPPING_RESOURCE = API + ACCESS_GROUP_MAPPING;
-    static final String ACCESS_GROUP_POST_ROLE = ACCESS_GROUP_MAPPING + "/post";
-    static final String ACCESS_GROUP_PUT_ROLE = ACCESS_GROUP_MAPPING + "/put";
-    static final String ACCESS_GROUP_GET_ROLE = ACCESS_GROUP_MAPPING + "/get";
-    static final String ACCESS_GROUP_DELETE_ROLE = ACCESS_GROUP_MAPPING + "/delete";
+    static final String ACCESS_GROUP_MAPPING_RESOURCE = SEPARATOR + ACCESS_GROUP_MAPPING;
+
+    private static final String BASE_ACCESS_GROUP_ROLE = ROOT + SEPARATOR + APP_IDENTIFIER + SEPARATOR + ACCESS_GROUP_MAPPING;
+    static final String ACCESS_GROUP_POST_ROLE = BASE_ACCESS_GROUP_ROLE + SEPARATOR + "post";
+    static final String ACCESS_GROUP_PUT_ROLE = BASE_ACCESS_GROUP_ROLE + SEPARATOR + "put";
+    static final String ACCESS_GROUP_GET_ROLE = BASE_ACCESS_GROUP_ROLE + SEPARATOR + "get";
+    static final String ACCESS_GROUP_DELETE_ROLE = BASE_ACCESS_GROUP_ROLE + SEPARATOR + "delete";
+
     /**
      * Usuários
      */
     private static final String USER_MAPPING = "users";
-    static final String USER_MAPPING_RESOURCE = API + USER_MAPPING;
-    static final String USER_POST_ROLE = USER_MAPPING + "/post";
-    static final String USER_PUT_ROLE = USER_MAPPING + "/put";
-    static final String USER_GET_ROLE = USER_MAPPING + "/get";
+    static final String USER_MAPPING_RESOURCE = SEPARATOR + USER_MAPPING;
 
-    /**
-     * Root
-     */
-    public static final String ROOT = "root";
-    static final String USER_PUT_ACTIVATE_ROLE = USER_PUT_ROLE + "/activate";
-    static final String USER_PUT_CHANGE_PASSWORD_ROLE = USER_PUT_ROLE + "/change-password";
+    private static final String BASE_USER_ROLE = ROOT + SEPARATOR + APP_IDENTIFIER + SEPARATOR + USER_MAPPING;
+    static final String USER_POST_ROLE = BASE_USER_ROLE + SEPARATOR + "post";
+    static final String USER_PUT_ROLE = BASE_USER_ROLE + SEPARATOR + "put";
+    static final String USER_GET_ROLE = BASE_USER_ROLE + SEPARATOR + "get";
+    static final String USER_PUT_ACTIVATE_ROLE = USER_PUT_ROLE + SEPARATOR + "activate";
+    static final String USER_PUT_CHANGE_PASSWORD_ROLE = USER_PUT_ROLE + SEPARATOR + "change-password";
 
     /**
      * Permissões
      */
     private static final String PERMISSION_MAPPING = "permissions";
-    static final String PERMISSION_MAPPING_RESOURCE = API + PERMISSION_MAPPING;
-    static final String PERMISSION_GET_ROLE = PERMISSION_MAPPING + "/get";
+    static final String PERMISSION_MAPPING_RESOURCE = SEPARATOR + PERMISSION_MAPPING;
+
+    private static final String BASE_PERMISSION_ROLE = ROOT + SEPARATOR + APP_IDENTIFIER + SEPARATOR + PERMISSION_MAPPING;
+    static final String PERMISSION_GET_ROLE = BASE_PERMISSION_ROLE + SEPARATOR + "get";
 
 }
--- microservices/api/functional/account-manager/src/main/resources/db/migrations/1.0.2__CREATING_UNNACENT_EXTENSION_AND_FILTER_FUNCTION.sql
+++ microservices/api/functional/account-manager/src/main/resources/db/migrations/1.0.2__CREATING_UNNACENT_EXTENSION_AND_FILTER_FUNCTION.sql
@@ -10,10 +10,10 @@ CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA pg_catalog;
 
 
 CREATE OR REPLACE FUNCTION filter(needles text, VARIADIC haystacks text [])
-RETURNS boolean AS $$
+    RETURNS boolean AS $$
 SELECT needles IS NULL OR trim(needles) = '' OR EXISTS(
-    SELECT DISTINCT 1
-    FROM unnest(haystacks) haystack,
-    unnest(string_to_array(needles, ',')) needle
-    WHERE unaccent(haystack) ILIKE '%' || unaccent(needle) || '%');
+        SELECT DISTINCT 1
+        FROM unnest(haystacks) haystack,
+             unnest(string_to_array(needles, ',')) needle
+        WHERE unaccent(haystack) ILIKE '%' || unaccent(needle) || '%');
 $$ LANGUAGE SQL;
