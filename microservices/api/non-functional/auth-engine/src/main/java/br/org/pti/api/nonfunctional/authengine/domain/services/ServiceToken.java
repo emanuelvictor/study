4823c48b4e6dc7059ea7f85443474afc7ed9be86
emanuel.fonseca <emanuel.fonseca@pti.org.br>
2020-08-31 16:28:43 -0300

    ref #13

--- microservices/api/non-functional/auth-engine/src/main/java/br/org/pti/api/nonfunctional/authengine/domain/services/ServiceToken.java
+++ microservices/api/non-functional/auth-engine/src/main/java/br/org/pti/api/nonfunctional/authengine/domain/services/ServiceToken.java
@@ -5,6 +5,10 @@ import br.org.pti.api.nonfunctional.authengine.domain.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.NullValueInNestedPathException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -12,6 +16,12 @@ import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeSe
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author emanuelvictor
@@ -36,6 +46,7 @@ public class ServiceToken {
      * @param refreshToken String
      */
     public void removeRefreshToken(final String refreshToken) {
+
         final OAuth2RefreshToken oAuth2RefreshToken = tokenStore.readRefreshToken(refreshToken);
 
         this.removeRefreshToken(oAuth2RefreshToken);
@@ -46,6 +57,8 @@ public class ServiceToken {
      */
     public void removeRefreshToken(final OAuth2RefreshToken oAuth2RefreshToken) {
 
+        this.notifyClients(oAuth2RefreshToken);
+
         tokenStore.removeAccessTokenUsingRefreshToken(oAuth2RefreshToken);
 
         tokenStore.removeRefreshToken(oAuth2RefreshToken);
@@ -54,9 +67,54 @@ public class ServiceToken {
     }
 
     /**
+     * @param oAuth2RefreshToken OAuth2RefreshToken
+     */
+    public void notifyClients(final OAuth2RefreshToken oAuth2RefreshToken) {
+
+        final OAuth2Authentication oAuth2Authentication = tokenStore.readAuthentication(oAuth2RefreshToken.getValue());
+
+        final Set<String> authorities = extractStringAuthorities(oAuth2Authentication);
+
+        authorities.forEach(System.out::println);
+
+    }
+
+    /**
+     * @param oAuth2Authentication OAuth2Authentication
+     * @return Set<String>
+     */
+    private static Set<String> extractStringAuthorities(final OAuth2Authentication oAuth2Authentication) {
+        Objects.requireNonNull(oAuth2Authentication);
+        return extractStringAuthorities(oAuth2Authentication.getUserAuthentication());
+    }
+
+    /**
+     * @param authentication Authentication
+     * @return Set<String>
+     */
+    private static Set<String> extractStringAuthorities(final Authentication authentication) {
+        Objects.requireNonNull(authentication);
+        return extractStringAuthorities(authentication.getAuthorities());
+    }
+
+    /**
+     * @param grantedAuthorities Collection<? extends GrantedAuthority>
+     * @return Set<String>
+     */
+    public static Set<String> extractStringAuthorities(final Collection<? extends GrantedAuthority> grantedAuthorities) {
+        Objects.requireNonNull(grantedAuthorities);
+        return grantedAuthorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .map(authority -> authority.contains("/") ? authority.substring(authority.indexOf("/") + 1) : authority).map(authority -> authority.contains("/") ? authority.substring(0, authority.indexOf("/")) : authority)
+                .filter(authority -> !authority.equals("root"))
+                .collect(Collectors.toSet());
+    }
+
+    /**
      * @param sessionId String
      */
     public void removeRefreshTokenBySessionId(final String sessionId) {
+
         final OAuth2RefreshToken oAuth2RefreshToken = ((JwtTokenStore) tokenStore).getRefreshTokenBySessionId(sessionId);
 
         this.removeRefreshToken(oAuth2RefreshToken);
--- microservices/api/non-functional/auth-engine/src/test/java/br/org/pti/api/nonfunctional/authengine/AuthorizationServerApplicationTests.java
+++ microservices/api/non-functional/auth-engine/src/test/java/br/org/pti/api/nonfunctional/authengine/AuthorizationServerApplicationTests.java
@@ -1,13 +1,32 @@
 package br.org.pti.api.nonfunctional.authengine;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.Assert;
 
-@SpringBootTest
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static br.org.pti.api.nonfunctional.authengine.domain.services.ServiceToken.extractStringAuthorities;
+
+//@SpringBootTest
 class AuthorizationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+        final Set<SimpleGrantedAuthority> authorities = Set.of(
+                new SimpleGrantedAuthority("root"),
+                new SimpleGrantedAuthority("root/account-manager/access-groups/put/activate"),
+                new SimpleGrantedAuthority("root/account-manager"),
+                new SimpleGrantedAuthority("root/financial"),
+                new SimpleGrantedAuthority("root/financial/insert-coin"),
+                new SimpleGrantedAuthority("root/falcatrua"),
+                new SimpleGrantedAuthority("root/falcatrua/outra")
+        );
+
+        Assert.isTrue("account-manager;financial;falcatrua".equals(String.join(";", extractStringAuthorities(authorities))), "");
+
+    }
 
 }
