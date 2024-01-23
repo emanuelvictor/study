05095bbc1abda28246d95ae52708475b8cdebc7f
EmanuelVictordeOlive <emanuelvictor@msn.com>
2024-01-23 00:10:44 -0300

    Add arithmetic operators

---
+++ lua/aprendendo-a-linguagem-java/2/2.1 Strings_concatenation.lua
@@ -0,0 +1,16 @@
+print("We can concat strings utilzing ,")
+print("A = 'test'")
+print("B = 'test2'")
+print([[print(A,B) must show:
+test    test2]])
+A = 'test'
+B = 'test2'
+print(A,B)
+print("We can use .. to concat strings without tabs")
+print("A = 'test'")
+print("B = 'test2'")
+print([[print(A..B) must show:
+testtest2]])
+A = 'test'
+B = 'test2'
+print(A..B)
