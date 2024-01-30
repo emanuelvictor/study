825f0989be942e3d3d0a1e7d07d81040d358fa45
EmanuelVictordeOlive <emanuelvictor@msn.com>
2024-01-30 00:45:17 -0300

    - Renaming wrong root package; - Going to excercises.

---
+++ lua/learning-lua-language/2/2.3.6 compilation.lua
@@ -0,0 +1,7 @@
+print('To compile use luac. Example: "luac someparameter file.lua".')
+print('The advantage is the bytecode generated cannot be read by user, because it\'s a binary file')
+print('We can use -o parameter to specify output file')
+print('We can use -v to show the version from de lua compiler')
+print('We can use -l to list bytecode')
+print('----------------------- let\'s try')
+print('luac 2.3.5 pause.lua')
